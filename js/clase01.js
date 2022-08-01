b4319a96c85483eb9282a5150f8f59d7d783ab44
Florencia Bravo <flor_bravo16@hotmail.com>
2022-07-31 22:02:11 -0300

    Se agrega el proyecto

---
+++ js/clase01.js
@@ -0,0 +1,15 @@
+ /*let apellido= "bravo"
+ let nombre= "florencia"
+ let anioNacimiento =  "1993-12-04"
+ let timbre
+ let empresa= nombre + "  " + apellido
+ */
+
+ //prompt ("ingrese su numero de dni")
+
+ debugger
+
+ for (let i=0;  i<21; i ++){
+    console.log("iteracion  Nro.", i)
+ }
+ 
